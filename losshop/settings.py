04638c3a52a56cc2a08fc6a5772e161c8a1e12f9
s7ntpenh <denismazur111@gmail.com>
2025-07-22 21:36:34 +0200

    add new app custom users

--- losshop/settings.py
+++ losshop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 86400 #30 days
 SESSION_SAVE_EVERY_REQUEST = True
+
+AUTH_USER_MODEL = 'users.CustomUser'
